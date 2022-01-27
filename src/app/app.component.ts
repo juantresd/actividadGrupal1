aa144b3b25dabbe7e1e891010fc6782379f4e8ea
MAC DISEÑO 2 <macdiseno2@iMac-de-MAC.cge.local>
2022-01-27 15:12:27 -0500

    agrega estilos bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularLoJ';
+  title = 'Omar';
+  nombre = 'Juan Carlos y Les';
 }
